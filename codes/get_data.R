62db59443cb046f9f0a09bdbc165dc6d73319a6d
abdumalik2014 <abdumalik2014@gmail.com>
2021-01-02 18:03:25 +0100

    finish converting

--- codes/get_data.R
+++ codes/get_data.R
@@ -78,7 +78,9 @@ drops <- c("MS.SubClass","MS.Zoning", "Street", "Alley", "Lot.Shape",
            "House.Style", "Overall.Qual", "Overall.Cond", "Roof.Style", 
            "Roof.Matl", "Exterior.1st", "Exterior.2nd", "Mas.Vnr.Type", 
            "Exter.Qual", "Exter.Cond", "Foundation", "Bsmt.Qual", 
-           "Bsmt.Cond", "Bsmt.Exposure")
+           "Bsmt.Cond", "Bsmt.Exposure", "BsmtFin.Type.1", "BsmtFin.Type.2",
+           "Heating", "Heating.QC", "Central.Air", "Electrical",
+           "Kitchen.Qual") 
 df <- df[ , !(names(df) %in% drops)]
 
 df$MS.SubClass.fact <- as.factor(df$MS.SubClass)
@@ -146,6 +148,81 @@ sum(df$Bsmt.Exposure == "")
 df$Bsmt.Exposure[df$Bsmt.Exposure ==''] <- NA
 df$Bsmt.Exposure.fact <- factor(df$Bsmt.Exposure, order = TRUE,
                             levels = c("No Basement", "No", "Mn", "Av", "Gd" ))
+
+df$BsmtFin.Type.1 <- df$BsmtFin.Type.1 %>% replace_na("No Basement")
+df$BsmtFin.Type.1[df$BsmtFin.Type.1 ==''] <- NA
+df$BsmtFin.Type.1.fact <- factor(df$BsmtFin.Type.1, order = TRUE,
+                                levels = c("No Basement", "Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"))
+
+df$BsmtFin.Type.2 <- df$BsmtFin.Type.2 %>% replace_na("No Basement")
+df$BsmtFin.Type.2[df$BsmtFin.Type.2 ==''] <- NA
+df$BsmtFin.Type.2.fact <- factor(df$BsmtFin.Type.2, order = TRUE,
+                                 levels = c("No Basement", "Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"))
+df$Heating.fact <- as.factor(df$Heating)
+df$Heating.QC.fact <- factor(df$Heating.QC, order = TRUE,
+                             levels = c("Po", "Fa", "TA", "Gd", "Ex"))
+df$Central.Air.fact <- as.factor(df$Central.Air)
+df$Electrical[df$Electrical ==''] <- NA
+df$Electrical.fact <- factor(df$Electrical, order = TRUE,
+                             levels = c("Mix", "FuseP", "FuseF", "FuseA", "SBrkr"))
+unique(df$Kitchen.Qual)
+df$Kitchen.Qual.fact <- factor(df$Kitchen.Qual, order = TRUE,
+                               levels = c("Po", "Fa", "TA", "Gd", "Ex"))
+unique(df$Functional)
+df$Functional.fact <- factor(df$Functional, order = TRUE,
+                             levels = c("Sal", "Sev", "Maj2", "Maj1", "Mod", "Min2", "Min1", "Typ"))
+
+unique(df$Fireplace.Qu)
+# 1422 houses with 0 Fireplaces
+# bsmt.exposure.empty.2 <- df[is.na(df$Fireplace.Qu),]
+df$Fireplace.Qu <- df$Fireplace.Qu %>% replace_na("No Fireplace")
+df$Fireplace.Qu.fact <- factor(df$Fireplace.Qu, order = TRUE,
+                               levels = c("No Fireplace","Po", "Fa", "TA", "Gd", "Ex"))
+
+# 157 houses has no garage
+sum(is.na(df$Garage.Type))
+df$Garage.Type <- df$Garage.Type %>% replace_na("No Garage")
+df$Garage.Type.fact <- as.factor(df$Garage.Type)
+
+# 2 houses has garage, but garage finish empty
+# garage.finish.empty <- df[df$Garage.Finish == "",]
+df$Garage.Finish <- df$Garage.Finish %>% replace_na("No Garage")
+df$Garage.Finish[df$Garage.Finish == ""] <- NA
+df$Garage.Finish.fact <- factor(df$Garage.Finish, order = TRUE,
+                                levels = c("Not given", "Unf", "RFn", "Fin"))
+
+df$Garage.Qual <- df$Garage.Qual %>% replace_na("No Garage")
+# 2 empty, 1 has garage
+# garage.qual.empty <- df[df$Garage.Qual == "",]
+df$Garage.Qual[df$Garage.Qual == ""] <- NA
+df$Garage.Qual.fact <- factor(df$Garage.Qual, order = TRUE,
+                               levels = c("No Garage","Po", "Fa", "TA", "Gd", "Ex"))
+
+df$Garage.Cond <- df$Garage.Cond %>% replace_na("No Garage")
+df$Garage.Cond[df$Garage.Cond == ""] <- NA
+df$Garage.Cond.fact <- factor(df$Garage.Cond, order = TRUE,
+                              levels = c("No Garage","Po", "Fa", "TA", "Gd", "Ex"))
+
+
+df$Paved.Drive.fact <- factor(df$Paved.Drive, order = TRUE,
+                              levels = c("N", "P", "Y"))
+df$Pool.QC<- df$Pool.QC %>% replace_na("No Pool")
+# 2917 houses has no pool. only 13 has
+sum(df$Pool.QC == "No Pool")
+df$Pool.QC.fact <- factor(df$Pool.QC, order = TRUE,
+                          levels = c("No Pool", "Fa", "TA", "Gd", "Ex"))
+
+df$Fence <- df$Fence %>% replace_na("No Fence")
+df$Fence.fact = factor(df$Fence, order = TRUE,
+                       levels = c("No Fence", "MnWw", "GdWo", "MnPrv", "GdPrv"))
+
+df$Misc.Feature <- df$Misc.Feature %>% replace_na("None")
+df$Misc.Feature.fact <- as.factor(df$Misc.Feature)
+
+df$Sale.Type.fact <- as.factor(df$Sale.Type)
+df$Sale.Condition.fact <- as.factor(df$Sale.Type)
+
+utils::View(garage.qual.empty)
 
 
 
